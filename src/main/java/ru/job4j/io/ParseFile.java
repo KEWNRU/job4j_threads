eb65a272da61d30477cc5b25ea41366983c76ce8
KEWNRU <n0kky21rus@gmail.com>
2024-07-01 18:10:39 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public class ParseFile {
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public String content(Predicate<Character> filter) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        try (InputStream input = new FileInputStream(file)) {
+            int data;
+            while ((data = input.read()) > 0) {
+                if (filter.test((char) data)) {
+                    sb.append((char) data);
+                }
+            }
+            return sb.toString();
+        }
+    }
+
+    public String getContent() throws IOException {
+        return content((data) -> true);
+    }
+
+    public String getContentWithoutUnicode() throws IOException {
+        return content((data) ->  data < 0x80);
+    }
+
+}
