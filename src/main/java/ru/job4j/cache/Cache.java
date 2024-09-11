9ced686a511590ff286d5b187ed7dd188c698bcf
KEWNRU <n0kky21rus@gmail.com>
2024-09-11 19:42:02 +0300

    1. Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,34 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.id(), model) == null;
+    }
+
+    public boolean update(Base model) throws OptimisticException {
+        return memory.computeIfPresent(model.id(), (id, storeg) -> {
+            if(model.version() != storeg.version()) {
+                throw new OptimisticException("Version mismatch");
+            }
+            return new Base(id, model.name(), storeg.version() + 1);
+        }) != null;
+    }
+
+    public void delete(int id) {
+        memory.remove(id);
+    }
+
+    public Optional<Base> findById(int id) {
+        return Stream.of(memory.get(id))
+                .filter(Objects::nonNull)
+                .findFirst();
+    }
+}
